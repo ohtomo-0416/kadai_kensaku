67a1fe925cff8f11a4ef873231221876b6ba7d89
ohtomo-0416 <ohtomotakuma0416@gmail.com>
2021-03-08 23:01:13 +0900

    更新

--- kensaku.php
+++ kensaku.php
@@ -20,8 +20,6 @@
                 <ul>
                     <li class="current"><a href="kensaku.html">検索フォーム</a></li>
                     <li><a href="kekka.html">検索結果</a></li>
-                    <li><a href="touroku.html">登録</a></li>
-                    <li><a href="sakujo.html">削除</a></li>
                 </ul>
 
             </nav>
