af815e95f293308b0c6d6340b183f0f79aac8b17
ohtomo-0416 <ohtomotakuma0416@gmail.com>
2021-03-09 03:29:03 +0900

    更新

--- kekka.php
+++ kekka.php
@@ -5,49 +5,22 @@
         header("Location: kensaku.html");
         exit();
     }else{
-        //名前入力判定
+
         if (!isset($_POST['namae'])  || $_POST['namae'] === "" ){
             $errors['name'] = "名前が入力されていません。";
         }
     }
      
-    if(count($errors) === 0){
         
         $dsn = 'mysql:host=localhost;dbname=sample01;charset=utf8';
         $user = 'root';
         $password = '';
      
-        try{
-            $dbh = new PDO($dsn, $user, $password);
-            $statement = $dbh->prepare("SELECT * FROM 学生表 WHERE 名前 LIKE '%" . $_POST["namae"] . "%' ");
-        
-            if($statement){
-                $yourname = $_POST['yourname'];
-                $like_yourname = "%".$yourname."%";
-                //プレースホルダへ実際の値を設定する
-                $statement->bindValue(':name', $like_yourname, PDO::PARAM_STR);
-                
-                if($statement->execute()){
-                    //レコード件数取得
-                    $row_count = $statement->rowCount();
-                    
-                    while($row = $statement->fetch()){
-                        $rows[] = $row;
-                    }
-                    
-                }else{
-                    $errors['error'] = "検索失敗しました。";
-                }
-                
-                //データベース接続切断
-                $dbh = null;	
+        $dbh = new PDO($dsn, $user, $password);
+            if(@$_POST["namae"] != ""){
+                $stmt = $pdo->query("SELECT * FROM 学生表 WHERE 名前 LIKE '%" . $_POST["namae"] . "%' ");
             }
         
-        }catch (PDOException $e){
-            print('Error:'.$e->getMessage());
-            $errors['error'] = "データベース接続失敗しました。";
-        }
-    }
 ?>
 
 <!DOCTYPE html>
@@ -73,34 +46,30 @@
             <h2>検索結果</h2>
             <table>
 
-            <th>学籍番号</th>
-            <th>名前</th>
-            <th>かな</th>
-            <th>性別</th>
-            <th>進路</th>
-            <th>課題</th>
-
-            <?php if (count($errors) === 0): ?>
+            <tr>
+                <th>学籍番号</th>
+                <th>名前</th>
+                <th>かな</th>
+                <th>性別</th>
+                <th>進路</th>
+                <th>課題</th>
+            </tr>
             
-            <?php 
-                foreach($rows as $row){
-            ?> 
             <tr> 
-	            <td><?=$row['学籍番号']?></td> 
-	            <td><?=htmlspecialchars($row['名前'],ENT_QUOTES,'UTF-8')?></td>
-                <td><?=htmlspecialchars($row['かな'],ENT_QUOTES,'UTF-8')?></td>
+            <?php foreach ($stmt as $row): ?>
+                <tr>
+
+                    <td><?php echo $row[0]?></td>
+                    <td><?php echo $row[1]?></td>
+                    <td><?php echo $row[2]?></td>
+                    <td><?php echo $row[3]?></td>
+                    <td><?php echo $row[4]?></td>
+
+                </tr>
+            
             </tr> 
-            <?php 
-                } 
-            ?>
- 
-            <?php elseif(count($errors) > 0): ?>
-            <?php
-                foreach($errors as $value){
-	            echo "<p>".$value."</p>";
-                }
-            ?>
-            <?php endif; ?>
+            
+            <?php endforeach; ?>
 
         </section>
 
